55e3ebb02726fd221aa5b9a6404b5885a77423fd
pankaj-dex <pankajgahlot16aug@gmail.com>
2025-07-11 13:02:46 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -16,7 +16,9 @@ import base64
 
 #Set up logging
 
-logging.basicConfig(level=logging.INFO) logger = logging.getLogger(name)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 #Load environment variables (API keys)
 
